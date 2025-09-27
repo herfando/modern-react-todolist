536574be6923451bf9e5f01398d675431a1a45e7
Herfando <herfando_91@yahoo.com>
2025-09-27 13:53:22 +0700

    create input for use komponent

--- src/components/ui/input/Input.tsx
+++ src/components/ui/input/Input.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React from 'react';
 
-const Input: React.FC = () => {
-   return <>Input.....</>;
-};
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className = '', type = 'text', ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        className={`
+          w-full p-4 bg-gray-800 border border-gray-700 rounded-lg 
+          text-white placeholder-gray-500 focus:outline-none focus:ring-2 
+          focus:ring-blue-600 focus:border-blue-600 transition-all duration-200
+          ${className}
+        `}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+
+Input.displayName = 'Input';
 
 export default Input;
