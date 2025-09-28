47ff016aaa5e26f7abbc08ff72d3ffc059e1c4ad
Herfando <herfando_91@yahoo.com>
2025-09-28 16:38:56 +0700

    edit URL connect to API

--- src/api/client.ts
+++ src/api/client.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 // Buat instance Axios
 const client = axios.create({
-  baseURL: 'https://api.your-backend.com', // Ganti dengan URL API backend Anda
+  baseURL: 'https://todolistbeformentee-production.up.railway.app', // Ganti dengan URL API backend Anda
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
